ea4dc7718d756872448f686c606f9442a00e1875
Dylan Uhryniuk <duhry99@gmail.com>
2021-09-22 18:14:09 -0500

    Assignment 1

---
+++ Assignment1/Assignment1_Q1.c
@@ -0,0 +1,17 @@
+/*
+ * Dylan Uhryniuk
+ * 3126716
+ * Assignment 1
+ * Q1
+ */
+#include<stdio.h>
+
+int main( void ) {
+	
+	printf("Beware the Jabberwock, my son\n");
+	printf(" The jaws that bite, the claws that catch!\n");
+	printf("Beware the Jubjub bird, and shun\n");
+	printf(" The frumious Bandersnatch!\n\n    -- Lewis Carroll");
+
+	return 0;
+}
---
+++ Assignment1/Assignment1_Q2.c
@@ -0,0 +1,13 @@
+/*
+ * Dylan Uhryniuk
+ * 3126716
+ * Assignment 1
+ * Q2
+ */
+#include<stdio.h>
+
+int main( void ) {
+	printf("   *\n  *|*\n *(|)*\n*******\n");
+	
+	return 0;
+}
